d37161d5220555c35d6fe3217d2d2a83b6c3098b
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-10-12 15:05:02 +0200

    Implement widget to the Company Customization menu

---
+++ src/Patches/HeraldryCreatorPanel_OnSaveClicked.cs
@@ -0,0 +1,22 @@
+namespace BTHeraldryColorHelper.Patches
+{
+    /// <summary>
+    /// After clicking "Save", update the activeDef values to match the color and crest pickers.
+    /// </summary>
+    [HarmonyPatch(typeof(BattleTech.UI.HeraldryCreatorPanel), "OnSaveClicked")]
+    public static class HeraldryCreatorPanel_OnSaveClicked
+    {
+        [HarmonyPrefix]
+        public static void Prefix(BattleTech.UI.HeraldryCreatorPanel __instance)
+        {
+            if (__instance.activeDef != null)
+            {
+                __instance.activeDef.primaryMechColorID = __instance.colorPicker.selectedPrimaryColorId;
+                __instance.activeDef.secondaryMechColorID = __instance.colorPicker.selectedSecondaryColorId;
+                __instance.activeDef.tertiaryMechColorID = __instance.colorPicker.selectedAccentColorId;
+                __instance.activeDef.textureLogoID = __instance.crestPicker.selectedCrestId;
+                __instance.activeDef.Refresh();
+            }
+        }
+    }
+}
